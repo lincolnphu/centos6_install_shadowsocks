4de53662512e857df73fe33c6b022d2f8780b64d
lincolnphu <10140765+lincolnphu@users.noreply.github.com>
2018-11-15 19:39:22 +0800

    Update centos6_install_ss.sh

--- centos6_install_ss.sh
+++ centos6_install_ss.sh
@@ -54,7 +54,7 @@ cp /tmp/shadowsocks-libev/rpm/SOURCES/etc/init.d/shadowsocks-libev /etc/init.d/
 cp /tmp/simple-obfs/debian/config.json /etc/shadowsocks-libev
 chmod +x /etc/init.d/shadowsocks-libev
 chkconfig --add shadowsocks-libev
-server shadowsocks-libev start
+sevice shadowsocks-libev start
 }
 compile_autoconfig
 compile_ss
