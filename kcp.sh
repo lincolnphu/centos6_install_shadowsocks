5c19d96cfc68c97404ac5761f453a376de5b7702
lincolnphu <10140765+lincolnphu@users.noreply.github.com>
2019-02-23 23:05:51 +0800

    Update kcp.sh

--- kcp.sh
+++ kcp.sh
@@ -4,6 +4,7 @@ wget https://github.com/xtaci/kcptun/releases/download/v20190109/kcptun-linux-38
 tar xvf kcptun-linux-386-20190109.tar.gz
 chmod +x ./server_linux_386
 mv server_linux_386 /usr/bin/
+rm -rf /usr/local/etc/kcptun-server-config.json
 wget -O /usr/local/etc/kcptun-server-config.json https://raw.githubusercontent.com/lincolnphu/centos6_install_shadowsocks/master/kcp.json
 wget https://raw.githubusercontent.com/lincolnphu/centos6_install_shadowsocks/master/kcp-server -O /etc/init.d/kcptun-server
 chmod +x /etc/init.d/kcptun-server
